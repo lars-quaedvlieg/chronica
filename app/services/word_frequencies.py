7358836af403d3b7e8a760110f780d78c8144917
lars-quaedvlieg <larsquaedvlieg@outlook.com>
2024-12-01 04:36:58 +0100

    srtopwords

--- app/services/word_frequencies.py
+++ app/services/word_frequencies.py
@@ -2,7 +2,7 @@ import json
 import os
 import re
 from collections import Counter
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 
 from app.routes.new_entry import NOTES_DIR
 
@@ -17,14 +17,14 @@ stopwords = {
     'few', 'for', 'from', 'further',
     'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's",
     'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself',
-    'let', "let's",
+    'let', "let's", 'like', 'yeah', 'uh', 'something',
     'me', 'more', 'most', "mustn't", 'my', 'myself',
     'no', 'nor', 'not',
     'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own',
     'same', 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such',
     'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too',
     'under', 'until', 'up',
-    'very',
+    'very', 'don',
     'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't",
     'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves'
 }
@@ -48,7 +48,7 @@ def generate_word_frequencies():
 
                 # Normalize and clean the text
                 words = re.findall(r'\b\w+\b', summary.lower())
-                word_counter.update([word for word in words if word not in stopwords])
+                word_counter.update([word for word in words if word not in stopwords.union(set(STOPWORDS)) and len(word) > 2])
 
     # Convert counter to a list of [word, frequency] pairs, sorted by frequency
     return word_counter
